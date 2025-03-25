a7d1556553be5c3833b3f74904a38c36cdf8410d
ggang89 <ggang89@gmail.com>
2025-03-25 17:19:25 +0900

    shadcn으로 form 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+// app/page.tsx
+import FormComponent from "../../components/FormComponent";
 
-
-export default function Home() {
+export default function Page() {
   return (
-    <>
-    
-    </>
+    <div className="p-50">
+      <h1>form</h1>
+      <FormComponent />
+    </div>
   );
 }
