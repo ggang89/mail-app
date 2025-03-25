68da199614f61e20b490c34f29864d65569033a9
ggang89 <ggang89@gmail.com>
2025-03-25 23:32:58 +0900

    form 컴포넌트에 input 추가

--- components/FormComponent.tsx
+++ components/FormComponent.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,18 +15,23 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+// 기본적인 객체 스키마 생성 => 유효성검증
 const formSchema = z.object({
   username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+    message: "2글자 이상 입력해주세요",
   }),
+  email: z.string().email({message: "이메일을 올바르게 입력해주세요"}),
+  password: z.string().min(6, {message: "6글자 이상 입력해주세요"}),
 });
 
 export default function FormComponent() {
-  // ...
+  // z.infer<typeof formSchema> => formSchema의 타입을 추출
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      email: "",
+      password: "",
     },
   });
 
@@ -45,13 +49,39 @@ export default function FormComponent() {
           name="username"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              {/* <FormLabel>Username</FormLabel> */}
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="name" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              {/* <FormLabel>email</FormLabel> */}
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+             
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              {/* <FormLabel>password</FormLabel> */}
+              <FormControl>
+                <Input placeholder="password" {...field} />
+              </FormControl>
+             
               <FormMessage />
             </FormItem>
           )}
