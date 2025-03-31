1f83e195de07975b3f676639b1bd440f577d0d27
ggang89 <ggang89@gmail.com>
2025-03-31 23:59:47 +0900

    env다시 설정

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,7 +1,5 @@
 export default async function getWeather() {
-  const res = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=35.1799528&lon=129.0752365&appid=686a73df8afa13ecc2c99704f6470090&units=metric"
-  );
+  const res = await fetch(process.env.WEATHER_URL);
   const data = await res.json();
   return data;
 }
