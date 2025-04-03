4877af1dce6eb65def72dbde7692327e5f8e6942
ggang89 <ggang89@gmail.com>
2025-04-03 17:56:10 +0900

    prisma client 설치 및 파일 생성

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+//lib/db.ts
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help#best-practices-for-using-prisma-client-in-development
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
