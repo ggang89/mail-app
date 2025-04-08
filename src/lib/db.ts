63df5ed9d520e26ae35aa1bf866356d5859984c7
ggang89 <ggang89@gmail.com>
2025-04-08 20:07:58 +0900

    prisma client로 새 user 생성해서 db에 넣어줌

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,3 @@
-//lib/db.ts
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help#best-practices-for-using-prisma-client-in-development
 
 import { PrismaClient } from "@prisma/client";
@@ -8,3 +7,22 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      id: 123,
+      email:'abc.abc.com',
+    }
+  })
+  console.log(user)
+ }
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
